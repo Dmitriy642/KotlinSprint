45a682a4e9b8bb5f566f36a0bf57f18cf0467925
Dmitriy <dmitriyshesterikov87@gmail.com>
2025-09-08 23:24:33 +0300

    KS-1-2 Yuri Gagarin

---
+++ src/main/kotlin/Lesson1_1/less1_task3.kt
@@ -0,0 +1,16 @@
+package org.example.Lesson1_1
+
+fun main(){
+  val year = 1961
+  var hour = 9
+  var minute = 7
+
+    println("takeoff year:$year")
+    println("takeoff hour:$hour")
+    println("takeoff minute:$minute")
+
+    hour = 10
+    minute = 55
+
+    print("$hour:$minute")
+}
