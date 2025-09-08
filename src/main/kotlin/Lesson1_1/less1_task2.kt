25c2bbb1260286c02bc8f6b43850659f3b549a25
Dmitriy <dmitriyshesterikov87@gmail.com>
2025-09-08 17:48:42 +0300

    KS-1-2 task about workers

---
+++ src/main/kotlin/Lesson1_1/less1_task2.kt
@@ -0,0 +1,15 @@
+package org.example.Lesson1_1
+
+fun main(){
+    val numberOfOrders = 10
+    val gratitudeOfPurchase = "Благодарствуем за преобретение!"
+
+    println(numberOfOrders)
+    println(gratitudeOfPurchase)
+
+    var workers = 2000
+//    println(workers)
+
+    workers = 1999
+    println(workers)
+}
