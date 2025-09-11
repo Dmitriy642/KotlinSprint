3d4180ca95620d7dd6d5195ca6c586e7f2d1ffb8
Dmitriy <dmitriyshesterikov87@gmail.com>
2025-09-11 18:07:16 +0300

    KS-2-4 task buff

---
+++ src/main/kotlin/Lesson_2/less2_task4.kt
@@ -0,0 +1,13 @@
+package org.example.Lesson_2
+
+fun main(){
+val minedDiamonds = 7
+val minedIron = 11
+    val baffDiamond = (minedDiamonds*2)/10
+    val baffIron = (minedIron*2)/10
+    println("Bonus diamonds: $baffDiamond")
+    println("Bonus iron: $baffIron")
+}
+
+
+
