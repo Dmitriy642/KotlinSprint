5aa4a74db6969026d472ddf02b64c8a16bdad98d
Dmitriy <dmitriyshesterikov87@gmail.com>
2025-09-11 13:21:16 +0300

    KS-1-5 corrected version

--- src/main/kotlin/Lesson1_1/less1_task5.kt
+++ src/main/kotlin/Lesson1_1/less1_task5.kt
@@ -1,12 +1,15 @@
 package org.example.Lesson1_1
 
-fun main(){
-   val seconds: Short = 6480
-   val hour =  seconds/3600
-   val minute = (seconds-(hour*3600))/60
-   val remainingSeconds = seconds - (minute*60+hour*3600)
+const val TOTAL_NUMBER_OF_SECOND = 6480
+const val SECONDS_IN_ONE_HOUR = 3600
+const val SECONDS_IN_ONE_MINUTE = 60
 
-        val time = String.format("%02d:%02d:%02d",hour, minute, remainingSeconds)
+fun main() {
+    val hour = TOTAL_NUMBER_OF_SECOND / SECONDS_IN_ONE_HOUR
+    val minute = (TOTAL_NUMBER_OF_SECOND - (hour * SECONDS_IN_ONE_HOUR)) / SECONDS_IN_ONE_MINUTE
+    val remainingSeconds = TOTAL_NUMBER_OF_SECOND - (minute * SECONDS_IN_ONE_MINUTE + hour * SECONDS_IN_ONE_HOUR)
+
+    val time = String.format("%02d:%02d:%02d", hour, minute, remainingSeconds)
 
     println(time)
 
