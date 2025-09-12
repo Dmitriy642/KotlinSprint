d1847e094aeeb845773a84a1d4569f064cb5e693
Dmitriy <dmitriyshesterikov87@gmail.com>
2025-09-12 17:47:46 +0300

    KS-2-2 correction task Salary

--- src/main/kotlin/Lesson1_1/less2_task2.kt
+++ src/main/kotlin/Lesson1_1/less2_task2.kt
@@ -1,12 +1,14 @@
 package org.example.Lesson1_1
 
-const val PERMANENT_EMPLOYEE = 50
-const val SALARY_PE = 30000
-const val INTERNS = 30
-const val SALARY_INTERNS = 20000
-const val TOTAL_NUMBER_OF_EMPLOYEES = 80
 
 fun main() {
+    val PERMANENT_EMPLOYEE = 50
+    val SALARY_PE = 30000
+    val INTERNS = 30
+    val SALARY_INTERNS = 20000
+
+    val TOTAL_NUMBER_OF_EMPLOYEES = 80
+
     val paymentsPermanentEmployee = PERMANENT_EMPLOYEE * SALARY_PE
     val totalExpenses = paymentsPermanentEmployee + (INTERNS * SALARY_INTERNS)
     val averageSalaryPerEmployee = totalExpenses / TOTAL_NUMBER_OF_EMPLOYEES
