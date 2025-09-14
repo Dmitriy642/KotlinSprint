6593e1678db3347f9093097467943c0fd6074ce7
Dmitriy <dmitriyshesterikov87@gmail.com>
2025-09-14 17:50:48 +0300

    KS-4-2 cargo transportation

---
+++ src/main/kotlin/Lesson_4/less4_task2.kt
@@ -0,0 +1,29 @@
+package org.example.Lesson_4
+
+fun main() {
+    System.setOut(java.io.PrintStream(System.out, true, "UTF-8"))
+    var weight = 20
+    var volume = 80
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': " +
+                "${
+                    (weight > MINIMUM_PERMISSIBLE_WEIGHT) and (weight <= MAXIMUM_PERMISSIBLE_WEIGHT)
+                            and (volume < MAXIMUM_PERMISSIBLE_VOLUME)
+                }"
+    )
+
+    weight = 50
+    volume = 97
+
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': " +
+                "${
+                    (weight > MINIMUM_PERMISSIBLE_WEIGHT) and (weight <= MAXIMUM_PERMISSIBLE_WEIGHT)
+                            and (volume < MAXIMUM_PERMISSIBLE_VOLUME)
+                }"
+    )
+}
+
+const val MINIMUM_PERMISSIBLE_WEIGHT = 35
+const val MAXIMUM_PERMISSIBLE_WEIGHT = 100
+const val MAXIMUM_PERMISSIBLE_VOLUME = 100
