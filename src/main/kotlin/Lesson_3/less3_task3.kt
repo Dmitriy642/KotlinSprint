7c87abe5b5f772b5b2cdaabb2c9988bc32b9fa0f
Dmitriy <dmitriyshesterikov87@gmail.com>
2025-09-14 12:27:24 +0300

    KS-3-3 adjustments were made

--- src/main/kotlin/Lesson_3/less3_task3.kt
+++ src/main/kotlin/Lesson_3/less3_task3.kt
@@ -2,16 +2,17 @@ package org.example.Lesson_3
 
 fun main() {
     val inputNumber = 7
-    println("$inputNumber x 1 = ${inputNumber * 1} \n" +
-            "$inputNumber x 2 = ${inputNumber * 2} \n" +
-            "$inputNumber x 3 = ${inputNumber * 3} \n" +
-            "$inputNumber x 4 = ${inputNumber * 4} \n" +
-            "$inputNumber x 5 = ${inputNumber * 5} \n" +
-            "$inputNumber x 6 = ${inputNumber * 6} \n" +
-            "$inputNumber x 7 = ${inputNumber * 7} \n" +
-            "$inputNumber x 8 = ${inputNumber * 8} \n" +
-            "$inputNumber x 9 = ${inputNumber * 9} \n"
-    )
+    val multiStr = """
+        $inputNumber x 1 = ${inputNumber * 1}
+        $inputNumber x 2 = ${inputNumber * 2}
+        $inputNumber x 3 = ${inputNumber * 3}
+        $inputNumber x 4 = ${inputNumber * 4}
+        $inputNumber x 5 = ${inputNumber * 5}
+        $inputNumber x 6 = ${inputNumber * 6}
+        $inputNumber x 7 = ${inputNumber * 7}
+        $inputNumber x 8 = ${inputNumber * 8}
+        $inputNumber x 9 = ${inputNumber * 9}
+    """.trimIndent()
+
+    println(multiStr)
 }
-
-
