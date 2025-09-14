9516acd58d64ff3ba1584e235907bbeb8ff1aced
Dmitriy <dmitriyshesterikov87@gmail.com>
2025-09-14 17:13:43 +0300

    KS-3-4 made amendments

--- src/main/kotlin/Lesson_3/less3_task5.kt
+++ src/main/kotlin/Lesson_3/less3_task5.kt
@@ -3,9 +3,9 @@ package org.example.Lesson_3
 fun main() {
     val dataString = "D2-D4;0"
     val split = dataString.split("-", ";")
-    val startOfMove = split.elementAt(0)
-    val endOfMove = split.elementAt(1)
-    val numberOfMove = split.elementAt(2)
+    val startOfMove = split[0]
+    val endOfMove = split[1]
+    val numberOfMove = split[2]
 
     println(startOfMove)
     println(endOfMove)
