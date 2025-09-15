53320013398bfff5e1dbae4bafa48053dd22f101
Dmitriy <dmitriyshesterikov87@gmail.com>
2025-09-15 16:37:41 +0300

    KS-4-3 revision

--- src/main/kotlin/Lesson_4/less4_task3.kt
+++ src/main/kotlin/Lesson_4/less4_task3.kt
@@ -2,18 +2,18 @@ package org.example.Lesson_4
 
 fun main() {
     System.setOut(java.io.PrintStream(System.out, true, "UTF-8"))
-    val weatherToday = SUNNY_WEATHER
-    val theAwningIsCurrently = THE_AWNING_IS_OPEN
+    val isSunnyToday = true
+    val isTheAwningOpen = true
     val airHumidity = 20
-    val timeOfYear = "Весна"
-    val favorableConditions = (weatherToday == SUNNY_WEATHER) and (theAwningIsCurrently == THE_AWNING_IS_OPEN) and
-            (airHumidity == PERMISSIBLE_AIR_HUMIDITY) and (timeOfYear != WINTER)
+    val timeOfYear = "Зима"
+    val favorableConditions = (isSunnyToday == IS_SUNNY_WEATHER) && (isTheAwningOpen == IS_AWNING_OPEN) &&
+            (airHumidity == PERMISSIBLE_AIR_HUMIDITY) && (timeOfYear != UNFAVORABLE_TIME_OF_YEAR)
 
     println(favorableConditions)
 
 }
 
-const val SUNNY_WEATHER = true
-const val THE_AWNING_IS_OPEN = true
+const val IS_SUNNY_WEATHER = true
+const val IS_AWNING_OPEN = true
 const val PERMISSIBLE_AIR_HUMIDITY = 20
-const val WINTER = "Зима"
+const val UNFAVORABLE_TIME_OF_YEAR = "Зима"
