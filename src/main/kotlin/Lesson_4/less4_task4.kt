519309d5f1263e9f694be2fe7a28d62d095b9744
Dmitriy <dmitriyshesterikov87@gmail.com>
2025-09-15 16:46:30 +0300

    KS-4-4 revision

--- src/main/kotlin/Lesson_4/less4_task4.kt
+++ src/main/kotlin/Lesson_4/less4_task4.kt
@@ -2,13 +2,12 @@ package org.example.Lesson_4
 
 fun main() {
     System.setOut(java.io.PrintStream(System.out, true, "UTF-8"))
-    val numberOfDay = 5
-    val armsAndAbs = (numberOfDay % 2) != 0
-    val legsAndBack = (numberOfDay % 2) == 0
+    val numberOfDay = 2
+    val isEven = (numberOfDay % 2) == 0
     println(
-        "Упражнения для рук:$armsAndAbs\n" +
-                "Упражнения для ног:$legsAndBack\n" +
-                "Упражнения для спины:$legsAndBack\n" +
-                "Упражнения для пресса:$armsAndAbs"
+        "Упражнения для рук:${!isEven}\n" +
+                "Упражнения для ног:$isEven\n" +
+                "Упражнения для спины:$isEven\n" +
+                "Упражнения для пресса:${!isEven}"
     )
 }
